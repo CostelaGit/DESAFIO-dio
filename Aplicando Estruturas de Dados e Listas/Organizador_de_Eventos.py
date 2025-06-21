17675320dabc61e3dc3387c4b02a77dfb4d2b11f
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-06-20 22:35:57 -0300

    adicionando mais desafios

---
+++ Aplicando Estruturas de Dados e Listas/Organizador_de_Eventos.py
@@ -0,0 +1,18 @@
+# Dicionário para agrupar participantes por tema
+eventos = {}
+
+# Entrada do número de participantes
+n = int(input().strip())
+
+# TODO: Crie um loop para armazenar participantes e seus temas:
+
+for _ in range(n):
+    participante, tema = input().split(', ')
+    if tema not in eventos:
+        eventos[tema] = []
+    eventos[tema].append(participante)
+
+
+# Exibe os grupos organizados
+for tema, participantes in eventos.items():
+    print(f"{tema}: {', '.join(participantes)}")
